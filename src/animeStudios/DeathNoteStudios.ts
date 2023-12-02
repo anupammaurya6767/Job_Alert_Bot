368f60ebb03e911a74a58087915dd338a3e6cf56
Anupam Maurya <anupammaurya6767@gmail.com>
2023-12-02 10:21:12 +0530

    Update DeathNoteStudios.ts

--- src/animeStudios/DeathNoteStudios.ts
+++ src/animeStudios/DeathNoteStudios.ts
@@ -5,7 +5,7 @@ const resLimit = 100;
 const url = `https://www.amazon.jobs/en/search.json?radius=24km&facets%5B%5D=normalized_country_code&facets%5B%5D=normalized_state_name&facets%5B%5D=normalized_city_name&facets%5B%5D=location&facets%5B%5D=business_category&facets%5B%5D=category&facets%5B%5D=schedule_type_id&facets%5B%5D=employee_class&facets%5B%5D=normalized_location&facets%5B%5D=job_function_id&facets%5B%5D=is_manager&facets%5B%5D=is_intern&offset=0&result_limit=${resLimit}&sort=relevant&latitude=&longitude=&loc_group_id=&loc_query=India&base_query=Software%20Development&city=&country=IND&region=&county=&query_options=&`;
 const name = 'amazon';
 
-const deathHandler: Handler = {
+const deathHandler: Shinigami = {
     company: {
         name: name,
     },
@@ -18,7 +18,7 @@ const deathHandler: Handler = {
             return (Date.now()- Date.parse(job.posted_date)) / 1000 <= 2592000;
         } )
 
-        const result: Job[] = jobs.map(job => {
+        const result: AnimeEpisode[] = jobs.map(job => {
             const urlPrefix = 'https://amazon.jobs'
             return {
                 id: job.id,
