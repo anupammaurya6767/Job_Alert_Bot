970c5f403e92628546525bed8994ee1ccb597503
Anupam Maurya <anupammaurya6767@gmail.com>
2023-12-02 11:05:32 +0530

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -24,11 +24,11 @@ const unwantedDescriptions = [
 export function filterUnwantedEpisodes(episodes: AnimeEpisode[]) {
     return episodes.filter(episode => {
         for (const unwantedTitle of unwantedTitles) {
-            if (episode.title.toLowerCase().includes(unwantedTitle.toLowerCase()))
+            if (episode.name.toLowerCase().includes(unwantedTitle.toLowerCase()))
                 return false;
         }
         for (const unwantedDescription of unwantedDescriptions) {
-            if (episode.description.toLowerCase().includes(unwantedDescription.toLowerCase()))
+            if (episode.jd.toLowerCase().includes(unwantedDescription.toLowerCase()))
                 return false;
         }
         return true;
