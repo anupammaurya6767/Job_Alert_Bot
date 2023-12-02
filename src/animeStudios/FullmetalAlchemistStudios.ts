2ad07268b112449d2e19e8ed6418d7592f4ea84b
Anupam Maurya <anupammaurya6767@gmail.com>
2023-12-02 10:22:26 +0530

    Update FullmetalAlchemistStudios.ts

--- src/animeStudios/FullmetalAlchemistStudios.ts
+++ src/animeStudios/FullmetalAlchemistStudios.ts
@@ -4,14 +4,14 @@ import axios from 'axios';
 const url = 'https://careers.google.com/api/v3/search/?degree=BACHELORS&degree=MASTERS&distance=50&hl=en_US&jex=ENTRY_LEVEL&jlo=en_US&location=India&q=Software%20Engineer';
 const name = 'google';
 
-const fullmetalHandler: Handler = {
+const fullmetalHandler: Shinigami = {
     company: {
         name: name,
     },
     getJobs: async () => {
         const fullmetalAPIRes = await axios.get(url);
         const jobs: any[] = fullmetalAPIRes.data.jobs;
-        const result: Job[] = jobs.map(job => {
+        const result: AnimeEpisode[] = jobs.map(job => {
             return {
                 id: job.id,
                 name: job.title,
